fa3824ca51315dc2e18d0124e46ed214c089bb11
Winkeltje84 <bwinkel@gmail.com>
2017-06-05 11:23:57 +0200

    refactored function() into ES6 HASHROCKET

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 // create a new component that shoud produce some HTML
 // this const App is setting a function and is returning JSX: html looking code which is actually JS
 // we use const and not var because we do not expect this to change (we do not do "App = 5" lateron or so)
- const App = function() {
+ const App = () => {
    return <div>Hi!</div>;
  }
 
