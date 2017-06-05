454422e4ff1595172c2fadd9c679b90659033641
Winkeltje84 <bwinkel@gmail.com>
2017-06-05 14:05:49 +0200

    added a constructor that sets the initiated search result to this.state byt this.setState

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 const API_KEY = 'AIzaSyBBB93xpLMhtPLkEJ4EHpIGn664nELFfk0'
 
-YTSearch({key: API_KEY, term: 'scubadiving'}, (data) => {console.log(data)})
-
 // create a new component that shoud produce some HTML
 // this const App is setting a function and is returning JSX: html looking code which is actually JS
 // we use const and not var because we do not expect this to change (we do not do "App = 5" lateron or so)
- class App extends Component {
-   render() {
-     return (
-       <div>
-        <SearchBar />
-       </div>
-     )
-   }
+class App extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = { videos: [] }
+
+  YTSearch({key: API_KEY, term: 'scubadiving'}, (data) => {
+    this.setState({ videos: data})
+  }) // sets the intial state of videos to the search 'scubadiving'
+
+  console.log(this)
+}
+
+ render() {
+   return (
+     <div>
+      <SearchBar />
+     </div>
+   )
  }
+}
 
 // take this component's generated HTML and put it into the page (in the DOM)
 // the component App is rendered in the 'container' class div element in index.html
