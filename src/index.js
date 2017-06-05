73fd0b1e7bd12c24a59d160f59c9c20ee4078971
Winkeltje84 <bwinkel@gmail.com>
2017-06-05 12:11:03 +0200

    SearchBar now renders in Index.js - also replaced component files into components folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyBBB93xpLMhtPLkEJ4EHpIGn664nELFfk0'
 
 // create a new component that shoud produce some HTML
 // this const App is setting a function and is returning JSX: html looking code which is actually JS
 // we use const and not var because we do not expect this to change (we do not do "App = 5" lateron or so)
  const App = () => {
-   return <div>Hi!</div>;
+   return (
+     <div>
+      <SearchBar />
+     </div>
+   )
  }
 
 // take this component's generated HTML and put it into the page (in the DOM)
