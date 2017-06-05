80b1d7bd9eec9fae9ef2c451500830b8ef8dfdf9
Winkeltje84 <bwinkel@gmail.com>
2017-06-05 13:16:56 +0200

    this.state is now printed into the page with the use of a key "term". The value of that key is than printed with { this.state.term }

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  constructor(props) { // all js classed have a function called constructor, which is called automatically when a class is constructed
+    super(props) // the parent component is called with super()
+
+    this.state = { term: ''} // creating a new object and pushing it to this.state, in this case an empty string
+  }
+
   render() {
-    return <input onChange={ event => console.log(event.target.value)} />
+    // we ALLWAYS manipulate the state with this.setState!
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term: event.target.value })} />
+        Value of the input: { this.state.term }
+      </div>
+    )
   }
 }
 
